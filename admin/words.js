e4f19d3860898cb78db4c31b859adf380659a7f2
Robin Rainton <robin@rainton.com>
2020-05-09 08:50:21 +0200

    Additons from npx @iobroker/create-adapter

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'legrand-ecocompteur adapter settings': {
+        'en': 'Adapter settings for legrand-ecocompteur',
+        'de': 'Adaptereinstellungen für legrand-ecocompteur',
+        'ru': 'Настройки адаптера для legrand-ecocompteur',
+        'pt': 'Configurações do adaptador para legrand-ecocompteur',
+        'nl': 'Adapterinstellingen voor legrand-ecocompteur',
+        'fr': "Paramètres d'adaptateur pour legrand-ecocompteur",
+        'it': "Impostazioni dell'adattatore per legrand-ecocompteur",
+        'es': 'Ajustes del adaptador para legrand-ecocompteur',
+        'pl': 'Ustawienia adaptera dla legrand-ecocompteur',
+        'zh-cn': 'legrand-ecocompteur的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
